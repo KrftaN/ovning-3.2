53496c7fe93d6d1bbe6d020e46f99d29574c59fd
KraftaN <63770425+KrftaN@users.noreply.github.com>
2024-05-31 13:45:16 +0200

    Add project files.

---
+++ ovning 3.2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace övning_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? (j/n):");
+            string graduated = Console.ReadLine().ToLower();
+
+            Console.WriteLine("Hur gammal är du?");
+            int age = int.Parse(Console.ReadLine());
+
+            if (graduated == "j" && age < 22)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig.");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu.");
+            }
+        }
+    }
+}
